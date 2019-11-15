badb4c2b89b0ab055026e81646cbd6bba36c07ba
Jonathan Zia <zia@gatech.edu>
2019-11-15 17:20:59 -0500

    Updates to animation script

--- animate.m
+++ animate.m
@@ -1,81 +1,125 @@
-function animate(validation_data, reward, lambda, start_iteration, end_iteration, numSheep)
+function animate(validation_data, reward, lambda, varargin)
 
-% figure; hold on; grid on;
-if numSheep == 2
-    s1 = scatter(0, 0, 'or', 'MarkerFaceColor', 'r');
-    s2 = scatter(0, 0, 'or', 'MarkerFaceColor', 'r');
-else
-    s = scatter(0, 0, 'or', 'MarkerFaceColor', 'r');
+% Optional arguments:
+% - 'start'         Iteration at which to start animation   (default 1)
+% - 'end'           Iteration at which to stop animation
+% - 'sheep'         Number of sheep
+% - 'pause'         Duration to pause between steps         (default 0.25)
+% - 'angles'        Angles of possible directions (default 0:pi/4:7*pi/4)
+% - 'video'         Filepath to video file for saving       (default none)
+
+% Parse optional input arguments
+if ~isempty(varargin)
+    for arg = 1:length(varargin)
+        if strcmp(varargin{arg}, 'start'); start_iteration = varargin{arg + 1};
+        elseif strcmp(varargin{arg}, 'end'); end_iteration = varargin{arg + 1};
+        elseif strcmp(varargin{arg}, 'sheep'); numSheep = varargin{arg + 1};
+        elseif strcmp(varargin{arg}, 'pause'); pauseTime = varargin{arg + 1};
+        elseif strcmp(varargin{arg}, 'angles'); angles = varargin{arg + 1};
+        elseif strcmp(varargin{arg}, 'video'); vidFLAG = true; vidPath = varargin{arg + 1};
+        end
+    end
 end
-d = scatter(1, -1, 'ok', 'MarkerFaceColor', 'k'); M = 0.25;
-l1 = line([d.XData d.XData + M*cos(0)], [d.YData d.YData + M*sin(0)], 'Color', [0, 0, 0], 'LineWidth', 2);
-l2 = line([d.XData d.XData + M*cos(pi/4)], [d.YData d.YData + M*sin(pi/4)], 'Color', [0, 0, 0], 'LineWidth', 2);
-l3 = line([d.XData d.XData + M*cos(pi/2)], [d.YData d.YData + M*sin(pi/2)], 'Color', [0, 0, 0], 'LineWidth', 2);
-l4 = line([d.XData d.XData + M*cos(3*pi/4)], [d.YData d.YData + M*sin(3*pi/4)], 'Color', [0, 0, 0], 'LineWidth', 2);
-l5 = line([d.XData d.XData + M*cos(pi)], [d.YData d.YData + M*sin(pi)], 'Color', [0, 0, 0], 'LineWidth', 2);
-l6 = line([d.XData d.XData + M*cos(5*pi/4)], [d.YData d.YData + M*sin(5*pi/4)], 'Color', [0, 0, 0], 'LineWidth', 2);
-l7 = line([d.XData d.XData + M*cos(3*pi/2)], [d.YData d.YData + M*sin(3*pi/2)], 'Color', [0, 0, 0], 'LineWidth', 2);
-l8 = line([d.XData d.XData + M*cos(7*pi/4)], [d.YData d.YData + M*sin(7*pi/4)], 'Color', [0, 0, 0], 'LineWidth', 2);
 
+% Set defaults for optional input arguments
+if ~exist('start_iteration', 'var'); start_iteration = 1; end
+if ~exist('end_iteration', 'var'); end_iteration = size(validation_data, 1)/lambda; end
+if ~exist('numSheep', 'var'); numSheep = (size(validation_data, 2) - 2)/2; end
+if ~exist('pauseTime', 'var'); pauseTime = 0.25; end
+if ~exist('angles', 'var'); angles = 0:pi/4:7*pi/4; end
+if ~exist('vidFLAG', 'var'); vidFLAG = false; end
+
+% Set placeholder for sheep indicators and graph lines
+s = cell(numSheep, 1); l = cell(length(angles), 1);
+
+% Initialize the sheep indicators
+for i = 1:numSheep
+    s{i} = scatter(0, 0, 'or', 'MarkerFaceColor', 'r');
+end
+
+% Initialize dog indicator
+d = scatter(1, -1, 'ok', 'MarkerFaceColor', 'k'); M = 0.25;
+
+% Initialize lines for possible directions
+for i = 1:length(angles)
+    l{i} = line([d.XData d.XData + M*cos(angles(i))], [d.YData d.YData + M*sin(angles(i))], ...
+        'Color', [0, 0, 0], 'LineWidth', 2);
+end
+
+% Initialize boundary lines
 line([-1.5 1.5], [1, -1], 'Color', 'k', 'LineStyle', '--')
 line([-1.5 1.5], [1, 1], 'Color', 'k', 'LineWidth', 2)
 line([-1.5 1.5], [-1, -1], 'Color', 'k', 'LineWidth', 2)
 line([-1.5 -1.5], [-1, 1], 'Color', 'k', 'LineWidth', 2)
 line([1.5 1.5], [-1, 1], 'Color', 'k', 'LineWidth', 2)
 
+% Set limits on axes
 xlim([-1.5, 1.5]); ylim([-1.5, 1.5])
 
+% Initialize video
+if vidFLAG
+    vid = VideoWriter(vidPath);
+    vid.Quality = 100; vid.FrameRate = 10;
+    open(vid);
+end
+
+% For each iteration...
 for i = start_iteration:end_iteration
     
+    % Set placeholders for iteration
     startIdx = lambda*(i-1) + 1; endIdx = startIdx + (lambda - 1);
     data = validation_data(startIdx:endIdx, :);
     weights = reward(startIdx:endIdx, :);
     if ~isempty(find(isnan(weights(:)), 1)); continue; end
     
+    % For each step in the iteration
     for j = 1:lambda
         
+        % Set figure title
         title("Iteration: " + i + ", Step: " + j)
         
-        if numSheep == 2
-            s1.XData = data(j, 1); s1.YData = data(j, 3);
-            s2.XData = data(j, 2); s2.YData = data(j, 4);
-            d.XData = data(j, 5); d.YData = data(j, 6);
-        else
-            s.XData = data(j, 1); s.YData = data(j, 2);
-            d.XData = data(j, 3); d.YData = data(j, 4);
-        end
-        l1.XData = [d.XData d.XData + M*cos(0)];
-        l1.YData = [d.YData d.YData + M*sin(0)];
-        l2.XData = [d.XData d.XData + M*cos(pi/4)];
-        l2.YData = [d.YData d.YData + M*sin(pi/4)];
-        l3.XData = [d.XData d.XData + M*cos(pi/2)];
-        l3.YData = [d.YData d.YData + M*sin(pi/2)];
-        l4.XData = [d.XData d.XData + M*cos(3*pi/4)];
-        l4.YData = [d.YData d.YData + M*sin(3*pi/4)];
-        l5.XData = [d.XData d.XData + M*cos(pi)];
-        l5.YData = [d.YData d.YData + M*sin(pi)];
-        l6.XData = [d.XData d.XData + M*cos(5*pi/4)];
-        l6.YData = [d.YData d.YData + M*sin(5*pi/4)];
-        l7.XData = [d.XData d.XData + M*cos(3*pi/2)];
-        l7.YData = [d.YData d.YData + M*sin(3*pi/2)];
-        l8.XData = [d.XData d.XData + M*cos(7*pi/4)];
-        l8.YData = [d.YData d.YData + M*sin(7*pi/4)];
-        col = weights(j, :) - min(weights(j, :));
-        col = col./max(col);
-        l1.Color = 1 - [col(1), col(1), col(1)];
-        l2.Color = 1 - [col(2), col(2), col(2)];
-        l3.Color = 1 - [col(3), col(3), col(3)];
-        l4.Color = 1 - [col(4), col(4), col(4)];
-        l5.Color = 1 - [col(1), col(1), col(1)];
-        l6.Color = 1 - [col(2), col(2), col(2)];
-        l7.Color = 1 - [col(3), col(3), col(3)];
-        l8.Color = 1 - [col(4), col(4), col(4)];
+        % Initialize counter for parsing validation data
+        counter = 1;
         
-        pause(0.5)
+        % Update the x and y data for each sheep
+        for k = 1:numSheep
+            s{k}.XData = data(j, counter); counter = counter + 1;
+        end
+        for k = 1:numSheep
+            s{k}.YData = data(j, counter); counter = counter + 1;
+        end
+        
+        % Update the x and y data for the dog
+        d.XData = data(j, counter); counter = counter + 1;
+        d.YData = data(j, counter);
+        
+        % Update the movement lines
+        for k = 1:length(angles)
+            l{k}.XData = [d.XData d.XData + M*cos(angles(k))];
+            l{k}.YData = [d.YData d.YData + M*sin(angles(k))];
+        end
+        
+        % Update the weights such that the minimum value is 0
+        col = weights(j, :) - min(weights(j, :));
+        col = col./max(col);    % Normalize to range [0, 1]
+        
+        % Update the colors of each line based on the weights
+        for k = 1:length(angles)
+            l{k}.Color = 1 - [col(k) col(k) col(k)];
+        end
+        
+        % Record video frame
+        if vidFLAG; writeVideo(vid, getframe(gcf)); end
+        
+        % Pause animation
+        pause(pauseTime)
         
     end
     
 end
+
+% End video, if necessary
+if vidFLAG; close(vid); end
 
 end
 
